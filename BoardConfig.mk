6a08e08537a62f0d9a80e9d4f9b96451d69e0e11
AdarshGrewal <adarshgrewal@gmail.com>
2025-01-16 14:44:34 +0530

    duchamp: Include MIUI Camera
    Signed-off-by: zenin1504 <kayozzprojects@yandex.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,3 +219,6 @@ WPA_SUPPLICANT_VERSION := VER_0_8_X
 
 # Inherit the proprietary files
 include vendor/xiaomi/duchamp/BoardConfigVendor.mk
+
+# MIUI Camera
+include device/xiaomi/duchamp-miuicamera/BoardConfig.mk
--- device.mk
+++ device.mk
@@ -378,3 +378,6 @@ PRODUCT_PACKAGES += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/duchamp/duchamp-vendor.mk)
+
+# MIUI Camera
+$(call inherit-product, device/xiaomi/duchamp-miuicamera/device.mk)
